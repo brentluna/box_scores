5f3cc4b0e91c9a318506799afebbe81e971260b6
Brent Luna <brent.luna@instacart.com>
2020-08-01 12:52:57 -0700

    remove package-lock

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,8 +2,8 @@ export const dateToYYYYMMDD = (date) => {
   if (date instanceof Date) {
     return [
       date.getFullYear(),
-      String(date.getMonth() + 1).padStart(2, 0),
-      String(date.getDate()).padStart(2, 0),
+      String(date.getMonth() + 1).padStart(2, '0'),
+      String(date.getDate()).padStart(2, '0'),
     ].join('');
   }
 
