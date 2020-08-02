d993028f850913599b35b235d68cd11f7ff400ce
Brent Luna <brent.luna@instacart.com>
2020-08-02 12:35:23 -0700

    set away team to be initial team

--- pages/game/[date]/[gid].tsx
+++ pages/game/[date]/[gid].tsx
@@ -7,7 +7,7 @@ import Header from '../../../components/header/Header';
 import { useRouter } from 'next/router';
 
 function Game({ game }) {
-  const [isHomeTeam, setHomeTeam] = useState(true);
+  const [isHomeTeam, setHomeTeam] = useState(false);
   const router = useRouter();
   return (
     <div>
