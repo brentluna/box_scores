189a4bf05f382168d89e60cefdefbe0d0cc2b9c7
Brent Luna <brent.luna@instacart.com>
2020-08-09 15:16:44 -0700

    add ip inof to test

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,4 +1,6 @@
 export default async (req, res) => {
+  const r = await fetch('http://ipinfo.io/json');
+  const d = await r.json();
   res.statusCode = 200;
-  res.json({ test: 'tested' });
+  res.json({ test: 'tested', ip: d });
 };
