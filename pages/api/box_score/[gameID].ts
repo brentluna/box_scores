cc913c906689c54d96c2ff7c6c7740be004a4b7a
Brent Luna <brent.luna@instacart.com>
2020-08-13 11:14:46 -0700

    update to dynamic api route

--- pages/api/box_score/[gameID].ts
+++ pages/api/box_score/[gameID].ts
@@ -1,12 +1,12 @@
 // //schedule
 // request url: https://secure.espn.com/core/nba/schedule?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full
 // request url: https://secure.espn.com/core/nba/scoreboard?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full
-
+//401224779
 export default async (req, res) => {
-  const { date } = req.query;
-  const url = `https://secure.espn.com/core/nba/schedule${
-    date ? `/_/date/${date}` : ''
-  }?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full`;
+  const {
+    query: { gameID },
+  } = req;
+  const url = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/summary?event=${gameID}`;
   const resp = await fetch(url);
   const data = await resp.json();
   res.statusCode = 200;
--- pages/api/schedule/[date].ts
+++ pages/api/schedule/[date].ts
@@ -1,10 +1,14 @@
 // //schedule
 // request url: https://secure.espn.com/core/nba/schedule?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full
 // request url: https://secure.espn.com/core/nba/scoreboard?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full
-//401224779
+
 export default async (req, res) => {
-  const { gameID } = req.query;
-  const url = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/summary?event=${gameID}`;
+  const {
+    query: { date },
+  } = req;
+  const url = `https://secure.espn.com/core/nba/schedule${
+    date ? `/_/date/${date}` : ''
+  }?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full`;
   const resp = await fetch(url);
   const data = await resp.json();
   res.statusCode = 200;
