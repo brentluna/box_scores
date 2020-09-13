14cd6a1e4fb83a17385314472ec297af933a1f76
Brent Luna <brent.luna@instacart.com>
2020-09-12 19:05:32 -0700

    add date check to change api call

--- pages/api/schedule/[date].ts
+++ pages/api/schedule/[date].ts
@@ -6,23 +6,46 @@ export default async (req, res) => {
   const {
     query: { date },
   } = req;
-  // const url = `https://secure.espn.com/core/nba/schedule${
-  // date ? `/_/date/${date}` : ''
-  // }?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full`;
+  let games;
+  if (dateIsToday(date)) {
+    const url =
+      'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
+    const resp = await fetch(url);
+    const data = await resp.json();
+    games = data.events;
+  } else {
+    const url = `https://secure.espn.com/core/nba/schedule${
+      date ? `/_/date/${date}` : ''
+    }?xhr=1&render=true&device=desktop&country=us&lang=en&region=us&site=espn&edition-host=espn.com&site-type=full`;
 
-  const url =
-    'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
-  const resp = await fetch(url);
-  const data = await resp.json();
-  // const {
-  //   content: { schedule },
-  // } = data;
-  // const games = schedule[date].games;
-  const games = data.events;
-
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const {
+      content: { schedule },
+    } = data;
+    games = schedule[date].games;
+  }
   const formattedGames = games.map((game) => formatGame(game));
   res.statusCode = 200;
   res.json(formattedGames);
+};
+
+const dateIsToday = (date: string) => {
+  let formattedStr = '';
+  date.split('').forEach((el, idx) => {
+    const idxs = [3, 5];
+    if (idxs.includes(idx)) {
+      formattedStr += `${el}-`;
+    } else {
+      formattedStr += el;
+    }
+  });
+  const todayDate = new Date().toLocaleDateString();
+  const passedDate = new Date(
+    Date.parse(formattedStr + 'T00:00:00')
+  ).toLocaleDateString();
+
+  return todayDate === passedDate;
 };
 
 const formatGame = (game) => {
