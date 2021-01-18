360002547c2b811e98857070e37453324e2d0aae
Brent Luna <brent.luna@instacart.com>
2021-01-18 15:25:13 -0800

    add cors

--- pages/api/teams.ts
+++ pages/api/teams.ts
@@ -1,5 +1,28 @@
+import Cors from 'cors';
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+);
+
 export default async (req, res) => {
   try {
+    await cors(req, res);
     const teamsURL = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams`;
     const resp = await fetch(teamsURL);
     const data = await resp.json();
