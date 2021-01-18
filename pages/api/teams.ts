19dde520090f8480f67ea51cae57267bad881bdd
Brent Luna <brent.luna@instacart.com>
2021-01-18 15:14:10 -0800

    fix resposne

--- pages/api/teams.ts
+++ pages/api/teams.ts
@@ -1,12 +1,13 @@
 export default async (req, res) => {
   try {
     const teamsURL = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams`;
-    const res = await fetch(teamsURL);
-    const data = await res.json();
+    const resp = await fetch(teamsURL);
+    const data = await resp.json();
     console.log({ data });
-    return data;
+    res.statusCode = 200;
+    res.json(data);
   } catch (error) {
-    console.error(error);
-    return [];
+    res.statusCode = 500;
+    res.json({ error });
   }
 };
